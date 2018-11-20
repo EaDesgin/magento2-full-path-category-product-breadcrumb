951f9c9f0af385719c7f2758b34c467ea41e07ff
mardeleanu <mardeleanu@gmail.com>
2018-11-20 20:52:58 +0200

    Adaugare optiune bad categories

--- Block/FullBreadcrumbs.php
+++ Block/FullBreadcrumbs.php
@@ -36,29 +36,34 @@ class FullBreadcrumbs extends \Magento\Framework\View\Element\Template
         parent::__construct($context, $data);
     }
 
-    public function getProductBreadcrumbs()
+    public function getProductBreadcrumbs($bad_categories, $enabled)
     {
-        $separator = ' <span class="breadcrumbsseparator"></span> ';
-        $product = $this->registry->registry('current_product');
-        /** @var  $categoryIds  AttributeValue */
-        $categoryIds = $product->getCustomAttribute('category_ids')->getValue();
+        if ($enabled) {
+            $bad_categories_array = explode(',', str_replace(' ', '', $bad_categories));
+            $separator = ' <span class="breadcrumbsseparator"></span> ';
+            $product = $this->registry->registry('current_product');
+            /** @var  $categoryIds  AttributeValue */
+            $categoryIds = $product->getCustomAttribute('category_ids')->getValue();
 
-        $collection = $this->categoryCollection->create();
-        $filtered_colection = $collection
-            ->addFieldToSelect('*')
-            ->addFieldToFilter(
-                'entity_id',
-                ['in' => $categoryIds]
-            )
-            ->setOrder('level', 'ASC')
-            ->load();
+            $collection = $this->categoryCollection->create();
+            $filtered_colection = $collection
+                ->addFieldToSelect('*')
+                ->addFieldToFilter(
+                    'entity_id',
+                    ['in' => $categoryIds]
+                )
+                ->setOrder('level', 'ASC')
+                ->load();
 
-        $categories = '';
-        foreach ($filtered_colection as $categoriesData) {
-            $categories .= '<a href="' . $categoriesData->getUrl() . '">';
-            $categories .= $categoriesData->getData('name') . '</a>' . $separator;
+            $categories = '';
+            foreach ($filtered_colection as $categoriesData) {
+                if (!in_array($categoriesData->getId(), $bad_categories_array)) {
+                    $categories .= '<a href="' . $categoriesData->getUrl() . '">';
+                    $categories .= $categoriesData->getData('name') . '</a>' . $separator;
+                }
+            }
+            $home_url = '<a href="' . $this->_storeManager->getStore()->getBaseUrl() . '">Magazie</a>';
+            return $home_url . $separator . $categories . '<span>' . $product->getName() . '</span>';
         }
-        $home_url = '<a href="' . $this->_storeManager->getStore()->getBaseUrl() . '">Magazie</a>';
-        return $home_url . $separator . $categories . '<span>' . $product->getName() . '</span>';
     }
 }
---
+++ Helper/Data.php
@@ -0,0 +1,44 @@
+<?php
+namespace Eadesigndev\FullBreadcrumbs\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Framework\App\Helper\Context;
+use Magento\Store\Model\ScopeInterface;
+
+class Data extends AbstractHelper
+{
+    const ENABLED = 'ea_fullbreadcrumbs/fullbreadcrumbs/enabled';
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    public $config;
+
+    /**
+     * Data constructor.
+     * @param Context $context
+     */
+    public function __construct(
+        Context $context
+    ) {
+        $this->config = $context->getScopeConfig();
+        parent::__construct($context);
+    }
+
+    /**
+     * @param string $configPath
+     * @return bool
+     */
+    public function getConfig($configPath)
+    {
+        return $this->config->getValue(
+            $configPath,
+            ScopeInterface::SCOPE_STORE
+        );
+    }
+
+    public function isEnabled($enablefullbreadcrumbs = self::ENABLED)
+    {
+        return $this->getConfig($enablefullbreadcrumbs);
+    }
+}
