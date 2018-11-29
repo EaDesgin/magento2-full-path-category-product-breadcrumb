290e0abd362957aa5c6c46aa98c84a9e2913a219
mardeleanu <mardeleanu@gmail.com>
2018-11-29 13:30:11 +0200

    #2655: Updated helper data

--- Helper/BreadcrumbsData.php
+++ Helper/BreadcrumbsData.php
@@ -5,7 +5,7 @@ use Magento\Framework\App\Helper\AbstractHelper;
 use Magento\Framework\App\Helper\Context;
 use Magento\Store\Model\ScopeInterface;
 
-class Data extends AbstractHelper
+class BreadcrumbsData extends AbstractHelper
 {
     const ENABLED = 'ea_fullbreadcrumbs/fullbreadcrumbs/enabled';
 
